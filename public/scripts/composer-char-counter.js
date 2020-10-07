f59c18e8f4f86147a979ea1ad120ca3fc423e17f
Z Xian <alex.xian@hotmail.com>
2020-10-07 03:39:08 +0000

    added counter function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,12 @@
-$(document).ready(function() {
-  // --- our code goes here ---
-  console.log("yeet")
-});
+$("#tweet-text").keyup(function(){
+  var counter = $(this).closest(".new-tweet").find(".counter");
+  let remaining = 140 - $(this).val().length;
+    counter.text(remaining)
+    console.log(remaining)
+    if (remaining < 0) {
+      counter.addClass("red");
+    } else {
+      counter.removeClass("red");
+    }
+})
+console.log("sdlkfj")
